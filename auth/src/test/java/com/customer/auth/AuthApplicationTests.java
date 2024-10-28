f93779a828e7bfa1907f998de14906925b2a6295
1hasangunduz <hasangunduz1010@gmail.com>
2024-10-28 19:08:11 +0300

    İlk commit mesajı v.1

--- auth/src/test/java/com/customer/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/customer/auth/AuthApplicationTests.java
@@ -13,3 +13,16 @@ class AuthApplicationTests {
 
 
 }
+```
+
+The `@SpringBootTest` annotation is used to tell Spring Boot to look for a main configuration class (one with `@SpringBootApplication` for instance) and use that to start a Spring application context. You can also use the `@SpringBootTest` annotation to specify the configuration of the application context using the `classes` attribute or the `value` attribute.
+
+The `@Test` annotation tells JUnit that the public void method to which it is attached can be run as a test case. To run the method, JUnit first constructs a fresh instance of the class then invokes the annotated method.
+
+Next, you can run the test by right-clicking on the `AuthApplicationTests` class and selecting `Run As` > `JUnit Test`.
+
+### Step 5: Create a User Entity
+
+A user entity is a representation of a user in the database. In this step, you will create a user entity and a repository to interact with the database.
+
+Create a new package, `com.customer.auth.entity`, and a new class, `User`, in the `entity` package.
